2c31e64f73955e966cec45db504547822864e414
stor0095 <geemakunstorey@Geemakuns-MacBook-Pro.local>
2015-11-25 19:58:09 -0500

    Be Social - First Upload
    Initial upload to Git.

--- Be Social/ViewController.swift
+++ Be Social/ViewController.swift
@@ -7,10 +7,52 @@
 //
 
 import UIKit
+import iAd
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ADBannerViewDelegate {
+    
+    @IBOutlet var firstLabel: UILabel!
+    @IBOutlet var tapButtonLabel: UILabel!
+    @IBOutlet var firstButton: UIButton!
+    @IBOutlet var adBanner: ADBannerView!
+    @IBOutlet var adBanner1: ADBannerView!
+    
+    
+  
+    // http://www.ioscreator.com/tutorials/display-an-alert-view-in-ios8-with-swift //
+    
+    @IBAction func alertButton(sender: AnyObject) {
+        
+        let alertController = UIAlertController(title: "Shut off your phone and put it down.", message:
+            "Please. Try it!", preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title: "Okay!", style: UIAlertActionStyle.Default,handler: nil))
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+   
+     func bannerViewWillLoadAd(banner: ADBannerView!) {
+     }
+     
+    func bannerViewActionShouldBegin(banner: ADBannerView!, willLeaveApplication willLeave: Bool) -> Bool {
+        return true
+    }
+    
+    func bannerViewDidLoadAd(banner: ADBannerView!) {
+        adBanner.hidden = false
+        adBanner1.hidden = false
+        }
+     
+     func bannerView(banner: ADBannerView!, didFailToReceiveAdWithError error: NSError!) {
+          NSLog("Error! Ad did not load.")
+     }
 
     override func viewDidLoad() {
+        adBanner.hidden = true
+        adBanner1.hidden = true
+        adBanner.delegate = self
+        adBanner1.delegate = self
+        self.canDisplayBannerAds = true
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
